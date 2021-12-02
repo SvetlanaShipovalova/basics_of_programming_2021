c71bbcf62957d8429056fa33837c4e275e361f08
DanilaMSU <78799957+DanilaSysoev@users.noreply.github.com>
2021-12-02 15:30:26 +0300

    add tests for task 9

--- hw/1sem/task_9/src/test/FunctionalityTester.java
+++ hw/1sem/task_9/src/test/FunctionalityTester.java
@@ -26,7 +26,15 @@ public class FunctionalityTester {
         testNum = 1;
         allOk = true;
         return switch (className) {
-            default -> true;
+            //Раскомментируйте следующую строку, если сделали подзадачу 1
+            //case "task.DoubleLinkedListItem" -> testDoubleLinkedListItem();
+
+            //Раскомментируйте следующую строку, если сделали подзадачу 2
+            //case "task.DoubleLinkedList" -> testDoubleLinkedList();
+
+            //Раскомментируйте следующую строку, если сделали подзадачу 3
+            //case "task.DoubleLinkedListSorter" -> testDoubleLinkedListSorter();
+            default -> false;
         };
     }
 
@@ -34,234 +42,181 @@ public class FunctionalityTester {
         allOk = allOk && testRes;
         protocol.append("\tТест ").append(testNum++).append(": ").append(testRes ? "ОК\n" : "Ошибка\n");
     }
+    void protoException() {
+        protocol.append("\tТест ").append(testNum++).append(" упал с ошибкой! ").append("Ошибка\n");
+    }
 
+
+    //Раскомментируйте следующий метод, если сделали подзадачу 1
+    /*
+    private boolean testDoubleLinkedListItem() {
+        DoubleLinkedListItem<Integer> item1 = new DoubleLinkedListItem<>(10);
+        writeProto(item1.getPrev() == null);
+        writeProto(item1.getNext() == null);
+        writeProto(item1.getData() == 10);
+        DoubleLinkedListItem<Integer> item2 = new DoubleLinkedListItem<>(20);
+        item1.setNext(item2);
+        item2.setPrev(item1);
+        writeProto(item1.getPrev() == null);
+        writeProto(item1.getNext() == item2);
+        writeProto(item2.getPrev() == item1);
+        writeProto(item2.getNext() == null);
+        writeProto(item2.getData() == 20);
+        return allOk;
+    }
+     */
+
+    //Раскомментируйте следующий блок, если сделали подзадачу 2
+    /*
+    private <T extends Comparable<T>> boolean listEquals(DoubleLinkedList<T> actual, Object[] expected) {
+        if(actual.getSize() != expected.length)
+            return false;
+        var worker = actual.getFirst();
+        for(int i = 0; i < expected.length; ++i) {
+            if(!worker.getData().equals(expected[i]))
+                return false;
+            worker = worker.getNext();
+        }
+        return true;
+    }
+    private boolean testDoubleLinkedList() {
+        try {
+            DoubleLinkedList<String> list = new DoubleLinkedList<>();
+            writeProto(list.getSize() == 0);
+            writeProto(list.getFirst() == null);
+            writeProto(list.getLast() == null);
+            list.insertAfter(null, "str1");
+            writeProto(list.getSize() == 1);
+            writeProto(list.getFirst().getData().equals("str1"));
+            writeProto(list.getLast().getData().equals("str1"));
+            writeProto(list.getFirst() == list.getLast());
+
+            var item = list.findFirstItem("str1");
+            writeProto(item.getData().equals("str1"));
+            writeProto(listEquals(list, new Object[] {"str1"}));
+
+            list.remove(item);
+            writeProto(list.getSize() == 0);
+            writeProto(list.getFirst() == null);
+            writeProto(list.getLast() == null);
+
+            list.insertAfter(null, "center");
+            item = list.findFirstItem("center");
+            list.insertAfter(item, "last");
+            list.insertAfter(null, "first");
+            writeProto(listEquals(list, new Object[] {"first", "center", "last"}));
+
+            list.insertAfter(item, "center2");
+            writeProto(listEquals(list, new Object[] {"first", "center", "center2", "last"}));
+            list.remove(list.findFirstItem("center2"));
+            writeProto(listEquals(list, new Object[] {"first", "center", "last"}));
+            list.remove(list.findFirstItem("first"));
+            writeProto(listEquals(list, new Object[] {"center", "last"}));
+            list.remove(list.findFirstItem("last"));
+            writeProto(listEquals(list, new Object[] {"center"}));
+            list.remove(list.findFirstItem("center"));
+            writeProto(listEquals(list, new Object[] {}));
+
+            list.insertBefore(null, "center");
+            item = list.findFirstItem("center");
+            list.insertBefore(null, "last");
+            list.insertBefore(item, "first");
+            list.insertBefore(item, "center2");
+            writeProto(listEquals(list, new Object[] {"first", "center2", "center", "last"}));
+            list.remove(list.findFirstItem("center2"));
+            writeProto(listEquals(list, new Object[] {"first", "center", "last"}));
+            list.remove(list.findFirstItem("first"));
+            writeProto(listEquals(list, new Object[] {"center", "last"}));
+            list.remove(list.findFirstItem("last"));
+            writeProto(listEquals(list, new Object[] {"center"}));
+            list.remove(list.findFirstItem("center"));
+            writeProto(listEquals(list, new Object[] {}));
+
+            list.insertAfter(null, "item");
+            item = list.findFirstItem("item");
+            list.insertAfter(item, "item");
+            list.insertAfter(item, "item");
+            list.insertAfter(item, "item");
+            list.insertAfter(item, "item");
+            writeProto(list.findFirstItem("item") == list.getFirst());
+            writeProto(list.findLastItem("item") == list.getLast());
+        }
+        catch (Exception e) {
+            protoException();
+            return false;
+        }
+        return allOk;
+    }
+     */
 
     //Раскомментируйте следующий метод, если сделали подзадачу 3
     /*
-    private boolean testInfantryman() {
-        Infantryman infantryman = new Infantryman("inf 1", 100, 20, 50);
-        writeProto(infantryman.name().equals("inf 1"));
+    private boolean testDoubleLinkedListSorter() {
+        try {
+            DoubleLinkedList<String> list = new DoubleLinkedList<>();
+            DoubleLinkedListSorter.sort(list);
+            writeProto(listEquals(list, new Object[]{}));
 
-        writeProto(infantryman.armor() == 50);
-        writeProto(infantryman.maxArmor() == 50);
-        writeProto(infantryman.health() == 100);
-        writeProto(infantryman.maxHealth() == 100);
-        writeProto(infantryman.strength() == 20);
-        writeProto(infantryman.baseStrength() == 20);
+            list = new DoubleLinkedList<>();
+            list.insertAfter(null, "d");
+            DoubleLinkedListSorter.sort(list);
+            writeProto(listEquals(list, new Object[]{"d"}));
 
-        infantryman.setMaxHealth(150);
-        writeProto(infantryman.health() == 100);
-        writeProto(infantryman.maxHealth() == 150);
+            list = new DoubleLinkedList<>();
+            list.insertAfter(null, "d");
+            list.insertAfter(null, "d");
+            DoubleLinkedListSorter.sort(list);
+            writeProto(listEquals(list, new Object[]{"d", "d"}));
 
-        infantryman.setMaxHealth(80);
-        writeProto(infantryman.health() == 80);
-        writeProto(infantryman.maxHealth() == 80);
+            list = new DoubleLinkedList<>();
+            list.insertAfter(null, "d");
+            list.insertAfter(null, "a");
+            list.insertAfter(null, "b");
+            list.insertAfter(null, "r");
+            list.insertAfter(null, "w");
+            list.insertAfter(null, "x");
+            list.insertAfter(null, "y");
+            DoubleLinkedListSorter.sort(list);
+            writeProto(listEquals(list, new Object[]{"a", "b", "d", "r", "w", "x", "y"}));
 
-        infantryman.setMaxHealth(100);
-        infantryman.heal(10);
-        writeProto(infantryman.health() == 90);
-        writeProto(infantryman.maxHealth() == 100);
-        infantryman.heal(20);
-        writeProto(infantryman.health() == 100);
-        writeProto(infantryman.maxHealth() == 100);
+            list = new DoubleLinkedList<>();
+            list.insertAfter(null, "a");
+            list.insertAfter(null, "a");
+            list.insertAfter(null, "b");
+            list.insertAfter(null, "a");
+            list.insertAfter(null, "a");
+            list.insertAfter(null, "a");
+            list.insertAfter(null, "a");
+            DoubleLinkedListSorter.sort(list);
+            writeProto(listEquals(list, new Object[]{"a", "a", "a", "a", "a", "a", "b"}));
 
-        infantryman.takeDamage(20);
-        writeProto(infantryman.health() == 80);
-        writeProto(infantryman.maxHealth() == 100);
-        infantryman.takeDamage(90);
-        writeProto(infantryman.health() == 0);
-        writeProto(infantryman.maxHealth() == 100);
+            list = new DoubleLinkedList<>();
+            list.insertAfter(null, "a");
+            list.insertAfter(null, "a");
+            list.insertAfter(null, "a");
+            list.insertAfter(null, "a");
+            list.insertAfter(null, "a");
+            list.insertAfter(null, "a");
+            list.insertAfter(null, "b");
+            DoubleLinkedListSorter.sort(list);
+            writeProto(listEquals(list, new Object[]{"a", "a", "a", "a", "a", "a", "b"}));
 
-        infantryman.setStrength(10);
-        writeProto(infantryman.baseStrength() == 20);
-        writeProto(infantryman.strength() == 10);
-        infantryman.setStrength(30);
-        writeProto(infantryman.baseStrength() == 20);
-        writeProto(infantryman.strength() == 30);
-
-        infantryman.setMaxArmor(40);
-        writeProto(infantryman.maxArmor() == 40);
-        writeProto(infantryman.armor() == 40);
-        infantryman.setMaxArmor(50);
-        writeProto(infantryman.maxArmor() == 50);
-        writeProto(infantryman.armor() == 40);
-        infantryman.damageArmor(30);
-        writeProto(infantryman.maxArmor() == 50);
-        writeProto(infantryman.armor() == 10);
-        infantryman.damageArmor(20);
-        writeProto(infantryman.maxArmor() == 50);
-        writeProto(infantryman.armor() == 0);
-        infantryman.restoreArmor(30);
-        writeProto(infantryman.maxArmor() == 50);
-        writeProto(infantryman.armor() == 30);
-        infantryman.restoreArmor(30);
-        writeProto(infantryman.maxArmor() == 50);
-        writeProto(infantryman.armor() == 50);
-
-        infantryman = new Infantryman("inf 1", 100, 20, 50);
-        Infantryman enemy = new Infantryman("enemy", 100, 10, 50);
-        infantryman.attack(enemy);
-        writeProto(enemy.health() == 90);
-        writeProto(enemy.armor() == 45);
-
-        infantryman = new Infantryman("inf 1", 100, 11, 50);
-        enemy = new Infantryman("enemy", 100, 10, 50);
-        infantryman.attack(enemy);
-        writeProto(enemy.health() == 95);
-        writeProto(enemy.armor() == 48);
-
-        infantryman = new Infantryman("inf 1", 100, 1, 50);
-        enemy = new Infantryman("enemy", 100, 10, 50);
-        infantryman.attack(enemy);
-        writeProto(enemy.health() == 99);
-        writeProto(enemy.armor() == 49);
-
+            list = new DoubleLinkedList<>();
+            list.insertAfter(null, "b");
+            list.insertAfter(null, "a");
+            list.insertAfter(null, "a");
+            list.insertAfter(null, "a");
+            list.insertAfter(null, "a");
+            list.insertAfter(null, "a");
+            list.insertAfter(null, "a");
+            DoubleLinkedListSorter.sort(list);
+            writeProto(listEquals(list, new Object[]{"a", "a", "a", "a", "a", "a", "b"}));
+        }
+        catch (Exception e) {
+            protoException();
+            return false;
+        }
         return allOk;
     }
-    */
-
-    //Раскомментируйте следующий метод, если сделали подзадачу 4
-    /*
-    private boolean testArmorDestroyer() {
-        ArmorDestroyer ad = new ArmorDestroyer("ad 1", 100, 20, 50);
-        Infantryman enemy = new Infantryman("enemy", 100, 10, 50);
-        ad.attack(enemy);
-        writeProto(enemy.health() == 95);
-        writeProto(enemy.armor() == 30);
-
-        ad = new ArmorDestroyer("ad 1", 100, 11, 50);
-        enemy = new Infantryman("enemy", 100, 10, 50);
-        ad.attack(enemy);
-        writeProto(enemy.health() == 98);
-        writeProto(enemy.armor() == 39);
-
-        ad = new ArmorDestroyer("ad 1", 100, 1, 50);
-        enemy = new Infantryman("enemy", 100, 10, 50);
-        ad.attack(enemy);
-        writeProto(enemy.health() == 99);
-        writeProto(enemy.armor() == 49);
-
-        BattleUnit[] enemies = new BattleUnit[] {
-            new Infantryman("enemy 1", 100, 10, 50),
-            new Infantryman("enemy 2", 100, 10, 45),
-            new Infantryman("enemy 3", 100, 10, 55)
-        };
-
-        ad = new ArmorDestroyer("ad 1", 100, 20, 50);
-        BattleUnit[] ownTeam = new BattleUnit[] {
-            ad
-        };
-        ad.specialAbility(ownTeam, enemies);
-        writeProto(enemies[2].armor() == 15);
-        writeProto(enemies[2].health() == 100);
-        writeProto(enemies[0].armor() == 50);
-        writeProto(enemies[0].health() == 100);
-        writeProto(enemies[1].armor() == 45);
-        writeProto(enemies[1].health() == 100);
-
-        enemies = new BattleUnit[] {
-            new Infantryman("enemy 1", 100, 10, 50),
-            new Infantryman("enemy 2", 100, 10, 45),
-            new Infantryman("enemy 3", 0, 10, 55)
-        };
-
-        ad = new ArmorDestroyer("ad 1", 100, 20, 50);
-        ownTeam = new BattleUnit[] {
-            ad
-        };
-        ad.specialAbility(ownTeam, enemies);
-        writeProto(enemies[2].armor() == 55);
-        writeProto(enemies[2].health() == 0);
-        writeProto(enemies[0].armor() == 10);
-        writeProto(enemies[0].health() == 100);
-        writeProto(enemies[1].armor() == 45);
-        writeProto(enemies[1].health() == 100);
-
-        enemies = new BattleUnit[] {
-            new Infantryman("enemy 1", 100, 10, 0),
-            new Infantryman("enemy 2", 100, 10, 0)
-        };
-
-        ad = new ArmorDestroyer("ad 1", 100, 20, 50);
-        ownTeam = new BattleUnit[] {
-            ad
-        };
-        ad.specialAbility(ownTeam, enemies);
-        writeProto(enemies[0].armor() == 0);
-        writeProto(enemies[1].armor() == 0);
-        writeProto(
-            (enemies[0].health() == 95 ||
-            enemies[1].health() == 95) &&
-            !(enemies[0].health() == 95 &&
-            enemies[1].health() == 95)
-        );
-
-
-        return allOk;
-    }
-    */
-
-    //Раскомментируйте следующий метод, если сделали подзадачу 5
-    /*
-    private boolean testAlchemist() {
-        Alchemist alchemist = new Alchemist("al 1", 100, 10, 20);
-        Infantryman enemy = new Infantryman("inf 1", 100, 10, 20);
-
-        alchemist.attack(enemy);
-        writeProto(enemy.strength() == 8);
-        writeProto(enemy.maxHealth() == 96);
-        writeProto(enemy.armor() == 20);
-
-        enemy = new Infantryman("inf 1", 100, 1, 20);
-        alchemist.attack(enemy);
-        writeProto(enemy.strength() == -1);
-        writeProto(enemy.maxHealth() == 96);
-        writeProto(enemy.armor() == 20);
-
-        enemy.attack(alchemist);
-        writeProto(alchemist.health() == 99);
-
-        alchemist = new Alchemist("al 1", 80, 10, 20);
-        BattleUnit[] enemies = new BattleUnit[] {
-            enemy
-        };
-        BattleUnit[] own = new BattleUnit[] {
-            alchemist,
-            new Infantryman("i1", 80, 10, 20),
-            new Infantryman("i1", 100, 10, 20),
-            new Infantryman("i1", 70, 10, 20)
-        };
-        own[3].takeDamage(20);
-        alchemist.specialAbility(own, enemies);
-        writeProto(own[0].health() == 80);
-        writeProto(own[1].health() == 80);
-        writeProto(own[2].health() == 100);
-        writeProto(own[3].health() == 60);
-
-        writeProto(own[0].strength() == 10);
-        writeProto(own[1].strength() == 10);
-        writeProto(own[2].strength() == 11);
-        writeProto(own[3].strength() == 10);
-
-        own = new BattleUnit[] {
-            alchemist,
-            new Infantryman("i1", 90, 10, 20),
-            new Infantryman("i1", 0, 10, 20),
-            new Infantryman("i1", 0, 10, 20)
-        };
-        own[0].takeDamage(20);
-        alchemist.specialAbility(own, enemies);
-        writeProto(own[0].health() == 70);
-        writeProto(own[1].health() == 90);
-        writeProto(own[2].health() == 0);
-        writeProto(own[3].health() == 0);
-
-        writeProto(own[0].strength() == 10);
-        writeProto(own[1].strength() == 11);
-        writeProto(own[2].strength() == 10);
-        writeProto(own[3].strength() == 10);
-
-        return allOk;
-    }
-    */
+     */
 }
