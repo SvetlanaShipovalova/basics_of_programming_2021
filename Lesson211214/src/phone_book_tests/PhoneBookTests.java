c20cbaca0dc9aeb40cd46a30cbfd30ab5189c37f
DanilaMSU <78799957+DanilaSysoev@users.noreply.github.com>
2021-12-14 16:19:20 +0300

    Создан тест create_CreateEmptyPhoneBook_ContactsCountEqualsZero

--- Lesson211214/src/phone_book_tests/PhoneBookTests.java
+++ Lesson211214/src/phone_book_tests/PhoneBookTests.java
@@ -3,5 +3,9 @@ package phone_book_tests;
 import org.junit.*;
 
 public class PhoneBookTests extends Assert {
-
+    @Test
+    public void create_CreateEmptyPhoneBook_ContactsCountEqualsZero() {
+        PhoneBook phoneBook = PhoneBook.create();
+        assertEquals(0, phoneBook.getContactsCount());
+    }
 }
